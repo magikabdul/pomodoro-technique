ccc5aaa070d7645a96fe6da80c420c0d0472d960
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-09-25 17:53:09 +0200

    [POM-13] buttons for control clock state

--- src/theme/MainTheme.js
+++ src/theme/MainTheme.js
@@ -1,8 +1,17 @@
 const theme = {
   color: {
+    white: '#fff',
+    background: {
+      primary: 'rgba(6,116,178,0.8)',
+      primaryNoOpacity: 'rgba(6,116,178)',
+      secondary: 'rgba(94,196,48,0.8)',
+      secondaryNoOpacity: 'rgb(94,196,48)',
+    },
+
     text: {
-      primary: '#f0f',
+      primary: '#d5e5f1',
     },
   },
 };
+
 export default theme;
